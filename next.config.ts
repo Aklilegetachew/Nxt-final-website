e897db1e44e1849dc73860fbab269a27a80475c5
Aklilegetachew <aklilegetachew@gmail.com>
2025-10-22 19:13:00 +0300

    out of all slash

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,12 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
+
   output: "export",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  exportTrailingSlash: true,
 }
 
 export default nextConfig
